46e4ee6a8b4b14d643c243517514c709ca7a481b
Gabriel Brolo Tobar <gabrolo13@gmail.com>
2019-08-13 23:08:35 -0600

    User Creation

--- app/components/CreateUserComponent/CreateUserComponent.js
+++ app/components/CreateUserComponent/CreateUserComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Card, CardBody, CardTitle, CardText, Input, CardFooter, Button } from "reactstrap"
 
+import axios from 'axios'
+
 // import './receipt-component.css'
 
 class CreateUserComponent extends Component {
@@ -16,8 +18,36 @@ class CreateUserComponent extends Component {
         }
     }    
 
+    changeUserPermissions = (permission) => {
+        const { userPermissions } = this.state
+        
+        if (!userPermissions.includes(permission) && permission != 'null') {
+            userPermissions.push(permission)
+        }
+
+        this.setState({ userPermissions })
+    }
+
+    createUser = () => {
+        const { name, email, password, passwordValidation, userPermissions } = this.state
+
+        axios({
+            method: "POST",
+            url: "http://localhost:8080/users/create",
+            data: { name, email, password, userPermissions: JSON.stringify(userPermissions) }
+        }).then(response => {
+            console.log(response)
+            if (response.data.code === 200 && response.data.status === "success") {
+                alert(response.data.message)
+            }
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
     render = () => {     
-        console.log(this.state)
+        const { name, email, password, passwordValidation } = this.state
+        
         return (
             <div className="receipt-component-container">
                 <Card>
@@ -27,13 +57,17 @@ class CreateUserComponent extends Component {
                         <Input onChange={(e) => this.setState({ email: e.target.value })} className="input-create" type="email" placeholder="Ingresa el correo" />
                         <Input onChange={(e) => this.setState({ password: e.target.value })} className="input-create" type="password" placeholder="Ingresa la contraseña" />
                         <Input onChange={(e) => this.setState({ passwordValidation: e.target.value })} className="input-create" type="password" placeholder="Ingresa la contraseña nuevamente" />
-                        <Input className="input-create" type="select" >
-                            <option>Selecciona</option>
+                        {
+                            password != passwordValidation &&
+                            <CardText style={{ color: 'red' }}>Las contraseñas deben ser iguales</CardText>
+                        }
+                        <Input onChange={(e) => this.changeUserPermissions(event.target.value)} className="input-create" type="select" >
+                            <option value="null">Selecciona permisos</option>
                             <option value="all-access">all-access</option>                            
                         </Input>
                     </CardBody>
                     <CardFooter>
-                        <Button>Crear usuario</Button>
+                        <Button onClick={() => this.createUser()}>Crear usuario</Button>
                     </CardFooter>
                 </Card>
             </div>
